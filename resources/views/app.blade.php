20650dc15f68cb9243ff1bba992183e375850ca7
Joshuawilfred <joewilfred216@gmail.com>
2024-07-05 02:37:45 +0300

    Updated views

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -1,11 +1,3 @@
-<x-layout>
-    <h1 class="text-primary">
-        Hello world!
-    </h1>
-    <div>
-        <p>
-            hello there
-        </p>
-    </div>
-    </body>
-</x-layout>
+<x-layouts.main>
+    
+</x-layouts.main>
--- resources/views/pages/contact.blade.php
+++ resources/views/pages/contact.blade.php
@@ -1,3 +1,3 @@
-<x-layout>
+<x-layouts.main>
     
-</x-layout>
+</x-layouts.main>
