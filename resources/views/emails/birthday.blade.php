d1ff86a9ca356b151ec68f1b86930dfd63f21204
Joshuawilfred <joewilfred216@gmail.com>
2024-07-04 14:55:53 +0300

    Simple mail controller and template

---
+++ resources/views/emails/birthday.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Happy Birthday!</title>
+</head>
+
+<body>
+    <h1>Happy Birthday, Josh!</h1>
+    <p>We wish you a wonderful day filled with joy and happiness.</p>
+</body>
+
+</html>
