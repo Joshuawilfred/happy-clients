bf401eae77671853ec040d8c3e9739860f2e262d
Joshuawilfred <joewilfred216@gmail.com>
2024-07-05 02:39:04 +0300

    Setup Mailable

--- app/Mail/WelcomeClient.php
+++ app/Mail/WelcomeClient.php
@@ -9,7 +9,7 @@ use Illuminate\Mail\Mailables\Content;
 use Illuminate\Mail\Mailables\Envelope;
 use Illuminate\Queue\SerializesModels;
 
-class SendBirthDayEmails extends Mailable
+class WelcomeClient extends Mailable
 {
     use Queueable, SerializesModels;
 
@@ -27,7 +27,7 @@ class SendBirthDayEmails extends Mailable
     public function envelope(): Envelope
     {
         return new Envelope(
-            subject: 'Happy Birthday',
+            subject: 'Welcome Client',
         );
     }
 
@@ -37,7 +37,7 @@ class SendBirthDayEmails extends Mailable
     public function content(): Content
     {
         return new Content(
-            view: 'emails.birthday',
+            view: 'emails.welcome',
         );
     }
 
