b3b5aba612de3a5edf5c6769efccfd002cb7dbb0
Joshuawilfred <joewilfred216@gmail.com>
2024-07-06 11:42:49 +0300

    GET and display upcomming holidays

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -39,5 +39,43 @@
             </x-tables.table>
         </div>
     </section>
+
+    <br><br>
+
+    <section>
+        <h2 class="px-3 text-kichwa-cha-habari">Upcoming Holidays</h2>
+        <div>
+            <x-tables.table>
+                <x-tables.head>
+                    <x-tables.row>
+                        <x-tables.heading>Name</x-tables.heading>
+                        <x-tables.heading>Date</x-tables.heading>
+                        <x-tables.heading>Status</x-tables.heading>
+                    </x-tables.row>
+                </x-tables.head>
+                <x-tables.body>
+                    @foreach ($holidays as $holiday)
+                        <x-tables.row>
+                            <x-tables.data>
+                                <span class="font-bold text-primary text-sm">{{ $holiday->name }}</span>
+                            </x-tables.data>
+
+                            <x-tables.data>
+                                {{ $holiday->date }}
+                            </x-tables.data>
+
+                            <x-tables.data>
+                                @if (2 + 2 == 0)
+                                    <x-buttons.danger-btn> Not Sent </x-buttons.danger-btn>
+                                @else
+                                    <x-buttons.success-btn> Sent</x-buttons.success-btn>
+                                @endif
+                            </x-tables.data>
+                        </x-tables.row>
+                    @endforeach
+                </x-tables.body>
+            </x-tables.table>
+        </div>
+    </section>
 </x-layouts.main>
 <x-layouts.footer />
